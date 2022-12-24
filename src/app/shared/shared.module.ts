84a0dcfc3fe9103d1c60b6eccfcef3b27b4107ab
Iñigo Molinuevo <i.m.parron@opendesuto.es>
2022-12-24 13:51:33 +0000

    Implement top panel template

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterPanelComponent } from './footer-panel/footer-panel.component';
 import { LoginPopupComponent } from './login-popup/login-popup.component';
+import { DropdownDirective } from './dropdown.directive';
 
 @NgModule({
-  declarations: [FooterPanelComponent, LoginPopupComponent],
+  declarations: [FooterPanelComponent, LoginPopupComponent, DropdownDirective],
   imports: [CommonModule],
-  exports: [FooterPanelComponent, LoginPopupComponent],
+  exports: [FooterPanelComponent, LoginPopupComponent, DropdownDirective],
 })
 export class SharedModule {}
