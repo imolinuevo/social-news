8a154fe8f184cbca2b53112482c8d03e4c92ea60
Iñigo Molinuevo <i.m.parron@opendesuto.es>
2022-12-27 10:47:56 +0000

    Finish trending component

--- src/app/main-panel/main-central-panel/trending-today/trending-today.component.ts
+++ src/app/main-panel/main-central-panel/trending-today/trending-today.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./trending-today.component.css'],
 })
 export class TrendingTodayComponent {
-  cardList = [1, 2, 3, 4];
+  cardList = [
+    {
+      id: 1,
+      title: 'Southwest Flight Cancellations',
+      description: 'ABC7 Los Angeles: Southwest Airlines cancels at...',
+      community: 'news',
+    },
+    {
+      id: 2,
+      title: 'Cody Gakpo',
+      description: 'OFFICIAL: PSV sells Cody Gakpo to Liverpool FC',
+      community: 'soccer',
+    },
+    {
+      id: 3,
+      title: 'George Santos',
+      description:
+        'George Santos Admits to Lying About College and Work History',
+      community: 'politics',
+    },
+    {
+      id: 4,
+      title: 'Nathaniel Hackett',
+      description:
+        '[Pelissero] The Broncos fired Nathaniel Hackett, per source.',
+      community: 'nfl',
+    },
+  ];
 }
