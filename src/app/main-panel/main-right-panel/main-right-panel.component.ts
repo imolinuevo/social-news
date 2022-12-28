376ce3b57810c06ed06669f4e56b2162e981c66e
Iñigo Molinuevo <i.m.parron@opendesuto.es>
2022-12-28 10:39:47 +0000

    Add initial template for main right panel

--- src/app/main-panel/main-right-panel/main-right-panel.component.ts
+++ src/app/main-panel/main-right-panel/main-right-panel.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main-right-panel',
   templateUrl: './main-right-panel.component.html',
-  styleUrls: ['./main-right-panel.component.css']
+  styleUrls: ['./main-right-panel.component.css'],
 })
 export class MainRightPanelComponent {
-
+  defaultPopularCommunities = [
+    'AskReddit',
+    'NoStupidQuestions',
+    'DestinyTheGame',
+    'explainlikeimfive',
+    'AskMen',
+    'leagueoflegends',
+    'Minecraft',
+  ];
 }
