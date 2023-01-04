be46483d0e2850ecc59feee3152aa2cb00313d19
Iñigo Molinuevo <i.m.parron@opendesuto.es>
2023-01-04 15:37:04 +0000

    Add basic template for post detail modal with navigation both in template and in component

--- src/app/post-detail-panel/post-detail-panel.component.ts
+++ src/app/post-detail-panel/post-detail-panel.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-detail-panel',
   templateUrl: './post-detail-panel.component.html',
-  styleUrls: ['./post-detail-panel.component.css']
+  styleUrls: ['./post-detail-panel.component.css'],
 })
 export class PostDetailPanelComponent {
+  constructor(private router: Router) {}
 
+  onClose() {
+    this.router.navigate(['/']);
+  }
 }
